081cefd67584c60d5064d5468842efe698ee5705
juan sebastian <juan.quiroz@gse.com.co>
2024-10-30 11:51:47 -0500

    first commit

---
+++ data/Puntosturisticos_3.js
@@ -0,0 +1,288 @@
+var json_Puntosturisticos_3 = [
+  {
+    features: [
+      {
+        type: "Feature",
+        properties: {
+          id: 1,
+          NOMBRE: "Barranquilla",
+          description:"Barranquilla, es la capital del departamento Atlántico de Colombia y es un desbordante puerto marino, bordeado por el río Magdalena. La ciudad es conocida por su enorme Carnaval, que reúne a artistas con extravagantes disfraces, carros elaborados y música cumbia.",
+          lugarA: "Zoológico de barranquilla",
+          DescripA:"El zoológico de Barranquilla mantiene diversas especies animales para la contemplación del público en Barranquilla, Colombia. Operado por la La Fundación Botánica y Zoológica de Barranquilla",
+          lugarB: "Monumento ventana del mundo",
+          DescripB:"La Ventana al mundo es un monumento público ubicado en Barranquilla, Colombia. Fue construido a finales de 2018 para coincidir con los XXIII Juegos Centroamericanos y del Caribe de los cuales la ciudad fue anfitriona.",
+          lugarC: "Estadio metropolitano",
+          DescripC:"El estadio Metropolitano Roberto Meléndez es un escenario para la práctica del fútbol y el atletismo en Barranquilla, Colombia. Fue inaugurado el 11 de mayo de 1986.",
+          lugarD: "Gran malecon del rio",
+          DescripD:"El Gran Malecón se extiende a lo largo de 5.5 kilómetros de la ribera occidental del río Magdalena y cuenta con un malecón y una vía en doble calzada paralela al mismo y con conexión con la vía 40 en las calles 79, 72 y 58 y se enlazará en la isla de La Loma y con el Centro de Eventos y Convenciones Puerta de Oro.",
+          img:[
+            "css/images/Turismo/Barranquilla/Barranquilla1.jpg",
+            "css/images/Turismo/Barranquilla/Barranquilla2.jpg",
+            "css/images/Turismo/Barranquilla/Barranquilla3.jpg",
+            "css/images/Turismo/Barranquilla/Barranquilla4.jpg"
+          ]},
+
+        geometry: {
+          type: "Point",
+          coordinates: [-74.8069007, 11.0041139],
+        },
+      },
+    ],
+  },
+  {
+    type: "FeatureCollection",
+    name: "Puntosturisticos_3",
+    crs: {
+      type: "name", properties: { name: "urn:ogc:def:crs:OGC:1.3:CRS84" },
+    },
+    features: [
+
+      {
+        type: "Feature",
+        properties: {
+          id: 2,
+          NOMBRE: "TUBARÁ",
+          lugarA: "Playas de Tubará",
+          DescripA:"Aunque Tubará no es directamente una ciudad costera, tiene cercanía a varias playas hermosas, como Playa Mendoza, muy apreciadas por los lugareños y turistas por su tranquilidad y ambiente relajado.",
+          lugarB: "Mirador de Tubará",
+          DescripB:"Desde este mirador, puedes disfrutar de vistas panorámicas del paisaje del Atlántico, con un entorno natural que abarca tanto montañas como valles y la costa en la distancia.",
+          lugarC: "Ciénaga de mallorquín",
+          DescripC:"bicada a poca distancia, esta ciénaga es un lugar perfecto para la observación de aves y el ecoturismo. Sus manglares y biodiversidad la hacen un destino único para los amantes de la naturaleza.",
+          lugarD: "Cascada del salto",
+          DescripD:"Este es un atractivo natural menos conocido pero muy hermoso, ubicado en las cercanías de Tubará. Es ideal para los que disfrutan del senderismo y los paisajes naturales, con un salto de agua refrescante.",
+          url: "https://www.google.com/maps?q=10.938377668367258, -75.02805926685055",
+          img:[
+            "https://i0.wp.com/notasdeactualidad.com/wp-content/uploads/2019/10/IMG-20191020-WA0044.jpg?w=1280&ssl=1",
+            "https://irp.cdn-website.com/760b2e87/dms3rep/multi/MPV-82.jpg",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRoHmMYM1bdDD1m63bSqSemYvvv1_Artgfdw&s",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRoHmMYM1bdDD1m63bSqSemYvvv1_Artgfdw&s",
+          ],
+          description:"Tubará, un pequeño municipio en el departamento del Atlántico, Colombia, ofrece algunos destinos turísticos interesantes, sobre todo para quienes buscan disfrutar de la naturaleza y la cultura local."},
+            
+        geometry: {
+          type: "Point",
+          coordinates: [-74.9770668, 10.872742],
+        },
+      },
+      // Uicación puntos de interés //
+      {
+        "type": "Feature",
+        "properties": {
+          "id": 3
+        },
+        "geometry": {
+          "type": "MultiPoint",
+          "coordinates": [
+            [-75.00118524295874, 10.905500180101544],
+            [-75.03028349925869, 10.94390813473088],
+            [-75.0427226206322, 10.905878531658818],
+            [-74.9298477474678, 10.88188499156902]
+          ]
+        }
+      }
+      
+    ],
+  },
+  {
+    type: "FeatureCollection",
+    name: "Puntosturisticos_3",
+    crs: {
+      type: "name", properties: { name: "urn:ogc:def:crs:OGC:1.3:CRS84" },
+    },
+    features: [
+      {
+        type: "Feature",
+        properties: {
+          id: 2,
+          NOMBRE: "JUAN DE ACOSTA ",
+          lugarA: "Lugar de interés 1",
+          DescripA:"",
+          lugarB: "Lugar de interés 2",
+          DescripB:"",
+          lugarC: "Lugar de interés 3",
+          DescripC:"",
+          lugarD: "Lugar de interés ",
+          DescripD:"",
+          img:[
+            "https://i0.wp.com/notasdeactualidad.com/wp-content/uploads/2019/10/IMG-20191020-WA0044.jpg?w=1280&ssl=1",
+            "https://irp.cdn-website.com/760b2e87/dms3rep/multi/MPV-82.jpg",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRoHmMYM1bdDD1m63bSqSemYvvv1_Artgfdw&s",
+          ],
+          description:
+            "Puerto Velero: Las playas del balneario de Puerto Velero se han convertido en un refinado destino turístico que brinda a los bañistas no sólo la belleza natural de su paisaje, sino además comodidad, orden y el espacio ideal para desarrollar actividades en el mar.",
+        },
+        geometry: {
+          type: "Point",
+          coordinates: [-75.035858,  10.829092],
+        },
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "id": 3
+        },
+        "geometry": {
+          "type": "MultiPoint",
+          "coordinates": [
+            [-75.09165521089542, 10.84929548649179],
+            [-75.07597840585062, 10.884672794344294],
+            [-75.066972581749, 10.819483578899543],
+            [-74.99859502838474, 10.808016670597278],
+          ]
+        }
+      }
+    ],
+  },
+  {
+    type: "FeatureCollection",
+    name: "Puntosturisticos_3",
+    crs: {
+      type: "name", properties: { name: "urn:ogc:def:crs:OGC:1.3:CRS84" },
+    },
+    features: [
+
+      {
+        type: "Feature",
+        properties: {
+          id: 2,
+          NOMBRE: "PIOJO",
+          lugarA: "Reserva natural los charcones",
+          DescripA:"",
+          lugarB: "Cerro la vieja ",
+          DescripB:"",
+          lugarC: "Calvaduria centro recreacional",
+          DescripC:"",
+          lugarD: "Taive",
+          DescripD:"",
+          img:[
+            "https://i0.wp.com/notasdeactualidad.com/wp-content/uploads/2019/10/IMG-20191020-WA0044.jpg?w=1280&ssl=1",
+            "https://irp.cdn-website.com/760b2e87/dms3rep/multi/MPV-82.jpg",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRoHmMYM1bdDD1m63bSqSemYvvv1_Artgfdw&s",
+          ],
+          description:
+            "Puerto Velero: Las playas del balneario de Puerto Velero se han convertido en un refinado destino turístico que brinda a los bañistas no sólo la belleza natural de su paisaje, sino además comodidad, orden y el espacio ideal para desarrollar actividades en el mar.",
+        },
+        geometry: {
+          type: "Point",
+          coordinates: [-75.107922,  10.748716],
+        },
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "id": 3
+        },
+        "geometry": {
+          "type": "MultiPoint",
+          "coordinates": [
+            [-75.09972311251174, 10.763008612014268],
+            [-75.1031256743147, 10.734833077230707],
+            [-75.09921672052812, 10.751651008366368],
+            [-75.0856986441673, 10.766867409611802]
+          ]
+        }
+      }
+    ],
+  },
+  {
+    type: "FeatureCollection",
+    name: "Puntosturisticos_3",
+    crs: {
+      type: "name", properties: { name: "urn:ogc:def:crs:OGC:1.3:CRS84" },
+    },
+    features: [
+
+      {
+        type: "Feature",
+        properties: {
+          id: 2,
+          NOMBRE: "LURUACO",
+          lugarA: "Reserva natural los charcones",
+          DescripA:"",
+          lugarB: "Cerro la vieja ",
+          DescripB:"",
+          lugarC: "Calvaduria centro recreacional",
+          DescripC:"",
+          lugarD: "Taive",
+          DescripD:"",
+          img:[
+            "https://i0.wp.com/notasdeactualidad.com/wp-content/uploads/2019/10/IMG-20191020-WA0044.jpg?w=1280&ssl=1",
+            "https://irp.cdn-website.com/760b2e87/dms3rep/multi/MPV-82.jpg",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRoHmMYM1bdDD1m63bSqSemYvvv1_Artgfdw&s",
+          ],
+          description:
+            "Puerto Velero: Las playas del balneario de Puerto Velero se han convertido en un refinado destino turístico que brinda a los bañistas no sólo la belleza natural de su paisaje, sino además comodidad, orden y el espacio ideal para desarrollar actividades en el mar.",
+        },
+        geometry: {
+          type: "Point",
+          coordinates: [-75.143244,  10.610414],
+        },
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "id": 3
+        },
+        "geometry": {
+          "type": "MultiPoint",
+          "coordinates": [
+            [-75.21757216612951, 10.66073669498964,],
+            [-75.24847121388426, 10.684353562949372],
+            [-75.08951277932373, 10.6600619003869],
+            [-75.09809584814448, 10.624295648008934,]
+          ]
+        }
+      },
+    ],
+  },
+  {
+    type: "FeatureCollection",
+    name: "Puntosturisticos_3",
+    crs: {
+      type: "name", properties: { name: "urn:ogc:def:crs:OGC:1.3:CRS84" },
+    },
+    features: [
+      {
+        type: "Feature",
+        properties: {
+          id: 2,
+          NOMBRE: "REPELÓN",
+          lugarA: "Reserva natural los charcones",
+          DescripA:"",
+          lugarB: "Cerro la vieja ",
+          DescripB:"",
+          lugarC: "Calvaduria centro recreacional",
+          DescripC:"",
+          lugarD: "Taive",
+          DescripD:"",
+          img:[
+            "https://i0.wp.com/notasdeactualidad.com/wp-content/uploads/2019/10/IMG-20191020-WA0044.jpg?w=1280&ssl=1",
+            "https://irp.cdn-website.com/760b2e87/dms3rep/multi/MPV-82.jpg",
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRoHmMYM1bdDD1m63bSqSemYvvv1_Artgfdw&s",
+          ],
+          description:
+            "Puerto Velero: Las playas del balneario de Puerto Velero se han convertido en un refinado destino turístico que brinda a los bañistas no sólo la belleza natural de su paisaje, sino además comodidad, orden y el espacio ideal para desarrollar actividades en el mar.",
+        },
+        geometry: {
+          type: "Point",
+          coordinates: [-75.123706,  10.494762],
+        },
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "id": 3
+        },
+        "geometry": {
+          "type": "MultiPoint",
+          "coordinates": [
+            [-75.07412851277972, 10.528326459360773,],
+            [-75.2380242201709, 10.55352882499517,],
+            [-75.21238925057801, 10.576250499053963,],
+            [-75.13002115155824, 10.416337006901125,]
+          ]
+        }
+      },
+    ],
+  },
+];
---
+++ js/Main.js
@@ -0,0 +1,346 @@
+/* *************** Cambio del la malla del mundo ******************* */
+
+//Selecciona las clases de los check de la malla
+
+
+let selectCheckMun = document.querySelectorAll('.leaflet-control-layers-selector');
+
+selectCheckMun.forEach(element => {
+    element.addEventListener("change",function (event) {
+        if (selectCheckMun[0].checked === true && selectCheckMun[1].checked === false) {
+            changeMun(0);
+        } else if (selectCheckMun[1].checked === true && selectCheckMun[0].checked === false) {
+            changeMun(1);
+        } else if (selectCheckMun[0].checked === true && selectCheckMun[1].checked === true){
+            changeMun(0);
+        } else {
+            layer_Mun_atlantico_2.remove();
+        }
+    })
+});
+
+function getColorById (id){
+    switch (id){
+        case 1: return "#AA3210"
+    }
+}
+
+
+function styleById (feature) {
+    return {
+        pane: 'municipios',
+        fillColor: getColorById(feature.properties.id),
+        fillOpacity: 0,
+        opacity: 1,
+        weight: 1,
+        interactive: true
+    };
+}
+
+function changeImage(imageSrc) {
+    const currentImage = document.getElementById('currentImage');
+    currentImage.src = imageSrc; // Cambia la fuente de la imagen principal
+}
+// Hover //
+function highlightFeature(e) {
+var layer = e.target;
+    layer.setStyle({
+        weight: 3,
+        color: "none",
+        fillOpacity: 0.3,
+    });
+}
+function resetHighlight(e) {
+    geojson.resetStyle(e.target);
+}
+
+function onEachFeature(feature, layer){
+    layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight
+    });
+}
+
+map.createPane("hover");
+map.getPane("hover").style.zIndex=500;
+
+map.createPane("municipios");
+map.getPane("municipios").style.zIndex=400;
+
+var geojson = L.geoJson(json_Mun_atlantico_2, {
+    style: styleById,
+    onEachFeature: onEachFeature,
+}).addTo(map);
+
+
+var layer_municipios_atlantico = L.geoJson(json_Mun_atlantico_2, {
+    style: styleById,
+}).addTo(map);
+
+
+map.fitBounds(layer_municipios_atlantico.getBounds());
+map.fitBounds(geojson.getBounds());
+
+function changeMun(valueJson) {
+
+    let style_Mun;
+
+    if (valueJson === 0) {
+        style_Mun = {
+            pane: 'pane_Mun_atlantico_2',
+            opacity: 1,
+            color: '#33691e',
+            // color: '#ffff',
+            dashArray: '',
+            lineCap: 'square',
+            lineJoin: 'bevel',
+            weight: 2.0,
+            fillOpacity: 0,
+            interactive: false,
+        }
+    } else if (valueJson === 1) {
+        style_Mun = {
+            pane: 'pane_Mun_atlantico_2',
+            opacity: 1,
+            // color: '#33691e',
+            color: '#ffff',
+            dashArray: '',
+            lineCap: 'square',
+            lineJoin: 'bevel',
+            weight: 2.0,
+            fillOpacity: 0,
+            interactive: false,
+        }
+    }
+
+    // //Elimina la malla del atlantico , ya creada
+    layer_Mun_atlantico_2.remove();
+
+    layer_Mun_atlantico_2 = new L.geoJson(json_Mun_atlantico_2[valueJson], {
+        attribution: '',
+        interactive: false,
+        dataVar: 'json_Mun_atlantico_2',
+        layerName: 'layer_Mun_atlantico_2',
+        pane: 'pane_Mun_atlantico_2',
+        style: style_Mun,
+    });
+
+    bounds_group.addLayer(layer_Mun_atlantico_2);
+    map.addLayer(layer_Mun_atlantico_2);
+}
+
+var layer_municipios_atlantico = L.geoJson(json_Mun_atlantico_2, {
+    style: styleById,
+    onEachFeature: onEachFeature
+}).addTo(map);
+
+/* ************ Modal y Cambio de puntos y rutas  ***************** */
+
+// Obtener el valor del input
+let selectOpcion = document.getElementById("selectOpcion");
+
+
+// Escuchar el evento 'message' para recibir datos de la página principal
+window.addEventListener('message', function(event) {
+
+    const mensaje = event.data;
+
+    changeRouteSpot(mensaje)
+});
+
+
+function changeRouteSpot(value) {
+
+    let jsonSpot = null;
+    let jsonRoute = null;
+    let jsonpoint = null;
+
+    if (value === "value1"){
+        
+        jsonpoint = json_Puntosturisticos_3[0];
+
+    } else if (value === "value2") {
+        jsonpoint = json_Puntosturisticos_3[0];
+        jsonSpot = json_Puntosturisticos_3[1];
+        jsonRoute = json_Ruta_4[0];
+
+    } else if (value === "value3") {
+        jsonpoint = json_Puntosturisticos_3[0];
+        jsonSpot = json_Puntosturisticos_3[2];
+        jsonRoute = json_Ruta_4[1];
+
+    } else if (value === "value4") {
+        jsonpoint = json_Puntosturisticos_3[0];
+        jsonSpot = json_Puntosturisticos_3[3];
+        jsonRoute = json_Ruta_4[2];
+
+    } else if (value ==="value5"){
+        jsonpoint = json_Puntosturisticos_3[0];
+        jsonSpot = json_Puntosturisticos_3[4];
+        jsonRoute = json_Ruta_4[3];
+        
+    } else if (value ==="value6"){
+        jsonpoint = json_Puntosturisticos_3[0];
+        jsonSpot = json_Puntosturisticos_3[5];
+        jsonRoute = json_Ruta_4[4];
+    } else {
+        if (geojsonLayer) {
+            geojsonLayer.remove(); // Elimina si existe
+            geojsonLayer = null; // Restablece a null
+        }
+        if (layer_Ruta_4) {
+            layer_Ruta_4.remove(); // Elimina si existe
+            layer_Ruta_4 = null; // Restablece a null
+        }
+        return; // Sale de la función
+    }
+
+    //Remueve los marcardores
+    if (geojsonLayer){
+        geojsonLayer.remove();
+    }
+    
+
+    //Marcadores - puntos
+    geojsonPoint = new L.geoJson(jsonpoint, {
+        attribution: '',
+        interactive: true,
+        dataVar: 'json_Puntosturisticos_3',
+        layerName: 'geojsonPoint',
+        pane: 'pane_Puntosturisticos_3',
+        onEachFeature: modal_Puntosturisticos_4,
+        pointToLayer: function (feature, latlng) {
+            return L.marker(latlng, { icon: style_Puntosturisticos_3_0(feature) });
+        },
+    });
+    bounds_group.addLayer(geojsonPoint);
+    map.addLayer(geojsonPoint);
+
+
+
+    geojsonLayer = new L.geoJson(jsonSpot, {
+        attribution: '',
+        interactive: true,
+        dataVar: 'json_Puntosturisticos_3',
+        layerName: 'geojsonLayer',
+        pane: 'pane_Puntosturisticos_3',
+        onEachFeature: modal_Puntosturisticos_4,
+        pointToLayer: function (feature, latlng) {
+            return L.marker(latlng, { icon: style_Puntosturisticos_3_0(feature) });
+        },
+    });
+
+
+    bounds_group.addLayer(geojsonLayer);
+    map.addLayer(geojsonLayer);
+
+    //Remueve las rutas
+    if (layer_Ruta_4) {
+        layer_Ruta_4.remove();
+    }
+
+
+    //Rutas
+    if (jsonRoute){
+        layer_Ruta_4 = new L.geoJson(jsonRoute, {
+            attribution: '',
+            interactive: false,
+            dataVar: 'json_Ruta_4',
+            layerName: 'layer_Ruta_4',
+            pane: 'pane_Ruta_4',
+            // onEachFeature: pop_Ruta_4,
+            style: style_Ruta_4_0,
+        });
+    }
+    
+    bounds_group.addLayer(layer_Ruta_4);
+    map.addLayer(layer_Ruta_4);
+
+}
+changeRouteSpot(null);
+
+// Para mostrar el modal
+function showModal(name, img, description, url, lugarA, lugarB, lugarC, lugarD, DescripA, DescripB, DescripC, DescripD ) {
+    const modal = document.getElementById("myModal");
+    const placeName = document.getElementById("place-name");
+    const placeImg1 = document.getElementById("place-img1");
+    const placeImg2 = document.getElementById("place-img2");
+    const placeImg3 = document.getElementById("place-img3");
+    const placeImg4 = document.getElementById("place-img4");
+    const placethumb1 = document.getElementById ("thumb1");
+    const placethumb2 = document.getElementById ("thumb2");
+    const placethumb3 = document.getElementById ("thumb3");
+    const placethumb4 = document.getElementById ("thumb4");
+    const placeDescription = document.getElementById("place-description");
+    const lugar1 = document.getElementById("lugar1");
+    const lugar2 = document.getElementById("lugar2");
+    const lugar3 = document.getElementById("lugar3");
+    const lugar4 = document.getElementById("lugar4");
+    const descrip1 = document.getElementById("descrip1");
+    const descrip2 = document.getElementById("descrip2");
+    const descrip3 = document.getElementById("descrip3");
+    const descrip4 = document.getElementById("descrip4");
+    const placeUrl = document.getElementById("place-url");
+
+    placeName.textContent = name;
+    placeImg1.src = img[0];
+    placeImg2.src = img[1];
+    placeImg3.src = img[2];
+    placeImg4.src = img[3];
+    placethumb1.src = img[0];
+    placethumb2.src = img[1];
+    placethumb3.src = img[2];
+    placethumb4.src = img[3];
+    placeDescription.textContent = description;
+    lugar1.textContent = lugarA;
+    lugar2.textContent = lugarB;
+    lugar3.textContent = lugarC;
+    lugar4.textContent = lugarD;
+    descrip1.textContent = DescripA;
+    descrip2.textContent = DescripB;
+    descrip3.textContent = DescripC;
+    descrip4.textContent = DescripD;
+    placeUrl.href = url;
+
+    modal.style.display = "block";
+}
+
+// Cerrar el modal
+const modal = document.getElementById("myModal");
+const span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+/* ********* Carrusel *********** */
+
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlide(index) {
+    if (index >= slides.length) {
+        currentSlide = 0;
+    } else if (index < 0) {
+        currentSlide = slides.length - 1;
+    } else {
+        currentSlide = index;
+    }
+
+    const offset = -currentSlide *100 ;
+    document.querySelector('.carousel-container').style.transform = `translateX(${offset}%)`;
+}
+
+function moveSlide(n) {
+    showSlide(currentSlide + n);
+}
+
+
+showSlide(currentSlide);

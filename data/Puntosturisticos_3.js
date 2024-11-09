b4128f5fae35fa993b70c056f96fb35b05c0b212
juan sebastian <juan.quiroz@gse.com.co>
2024-11-08 22:11:24 -0500

    changes map modal

--- data/Puntosturisticos_3.js
+++ data/Puntosturisticos_3.js
@@ -10,10 +10,8 @@ var json_Puntosturisticos_3 = [
           description:`El lugar más pintoresco a orillas del Mar Caribe, famoso por su antiguo muelle, que en su época fue la puerta de entrada de la modernidad al país. Sus playas ofrecen arenas suaves y aguas que reflejan el carácter del Caribe, siendo un destino ideal para los amantes de los deportes náuticos. Aquí, la historia y la belleza natural se encuentran en un ambiente de alegría, con lugares emblemáticos como el Muelle 1888 y la Estación del Ferrocarril. Las artesanías en madera náufraga y la gastronomía local reflejan la cercanía al mar y la riqueza de sabores del Caribe."`,
           url:"https://tesorosdelatlantico.com/tesoro/pto-colombia/",
           img:[
-            "css/images/Turismo/Barranquilla/Barranquilla1.jpg",
-            "css/images/Turismo/Barranquilla/Barranquilla2.jpg",
-            "css/images/Turismo/Barranquilla/Barranquilla3.jpg",
-            "css/images/Turismo/Barranquilla/Barranquilla4.jpg"
+            "css/images/Turismo/1-PUERTO-COLOMBIA/1.P.COLOMBIA.jpg",
+            "css/images/Turismo/1-PUERTO-COLOMBIA/7.P.COLOMBIA.jpg"
           ],
           
         },
@@ -30,24 +28,13 @@ var json_Puntosturisticos_3 = [
         type: "Feature",
         properties: {
           id: 2,
-          NOMBRE: "BARRANQUILLA",
+          NOMBRE: "Barranquilla",
+          titular:"Donde el mar abraza el río",
           description:`La vibrante "Puerta de Oro de Colombia". Ciudad que late al ritmo del Río Magdalena y el Mar Caribe, famosa por su gente alegre y el calor de sus días. Es una ciudad moderna, con el Gran Malecón del Río, que irradia magia, y un legado histórico que se refleja en sus emblemáticos barrios. El Barrio Abajo representa sus tradiciones y raíces, mientras que El Prado enaltece su arquitectura. Barranquilla es hogar de personas cálidas y hospitalarias que reciben a todos con los brazos abiertos y una sonrisa.`,
-          titular:"DONDE EL MAR ABRAZA EL RÍO 1",
-          // lugarA: "Zoológico de barranquilla",
-          // DescripA:"El zoológico de Barranquilla mantiene diversas especies animales para la contemplación del público en Barranquilla, Colombia. Operado por la La Fundación Botánica y Zoológica de Barranquilla",
-          // lugarB: "Monumento ventana del mundo",
-          // DescripB:"La Ventana al mundo es un monumento público ubicado en Barranquilla, Colombia. Fue construido a finales de 2018 para coincidir con los XXIII Juegos Centroamericanos y del Caribe de los cuales la ciudad fue anfitriona.",
-          // lugarC: "Estadio metropolitano",
-          // DescripC:"El estadio Metropolitano Roberto Meléndez es un escenario para la práctica del fútbol y el atletismo en Barranquilla, Colombia. Fue inaugurado el 11 de mayo de 1986.",
-          // lugarD: "Gran malecon del rio",
-          // DescripD:"El Gran Malecón se extiende a lo largo de 5.5 kilómetros de la ribera occidental del río Magdalena y cuenta con un malecón y una vía en doble calzada paralela al mismo y con conexión con la vía 40 en las calles 79, 72 y 58 y se enlazará en la isla de La Loma y con el Centro de Eventos y Convenciones Puerta de Oro.",
-          
           url:"https://tesorosdelatlantico.com/tesoro/barranquilla/",
           img:[
-            "css/images/Turismo/Barranquilla/Barranquilla1.jpg",
-            "css/images/Turismo/Barranquilla/Barranquilla2.jpg",
-            "css/images/Turismo/Barranquilla/Barranquilla3.jpg",
-            "css/images/Turismo/Barranquilla/Barranquilla4.jpg"
+            "css/images/Turismo/2-BARRANQUILLA/1.BARRANQUILLA.jpg",
+            "css/images/Turismo/2-BARRANQUILLA/6.BARRANQUILLA.jpg"
           ],
           
         },
@@ -64,15 +51,13 @@ var json_Puntosturisticos_3 = [
         type: "Feature",
         properties: {
           id: 2,
-          NOMBRE: "LURUACO",
-          titular:"A SIMPLE VISTA, TODA UNA VISTA",
+          NOMBRE: "Luruaco",
+          titular:"A simple vitsa, toda una visita",
           description:`Un encantador rincón entre montañas y lagunas, reconocido por su patrimonio gastronómico. Su plato insignia, la Arepa e’ Huevo, es famoso en toda Colombia y atrae a los amantes de la cocina caribeña. Luruaco ofrece una naturaleza rica en paisajes, destacando la Laguna de Luruaco y San Juan de Tocagua, lugares serenos ideales para quienes buscan paz. Aquí, las tradiciones y el folclor se viven de manera auténtica y sencilla, con gente orgullosa de sus raíces, siempre dispuesta a compartir su cultura con los visitantes.`,
           url:"https://tesorosdelatlantico.com/tesoro/luruaco/",
           img:[
-            "css/images/Turismo/Barranquilla/Barranquilla1.jpg",
-            "css/images/Turismo/Barranquilla/Barranquilla2.jpg",
-            "css/images/Turismo/Barranquilla/Barranquilla3.jpg",
-            "css/images/Turismo/Barranquilla/Barranquilla4.jpg"
+            "css/images/Turismo/3-LURUACO/1.LURUACO.jpg",
+            "css/images/Turismo/3-LURUACO/5.LURUACO.jpg"
           ],
         },
         geometry: {
@@ -88,15 +73,13 @@ var json_Puntosturisticos_3 = [
         type: "Feature",
         properties: {
           id: 2,
-          NOMBRE: "BARANOA",
-          titular:"EL CORAZÓN ALEGRE DEL ATLÁNTICO",
+          NOMBRE: "Baranoa",
+          titular:"El corazón alegre del atlántico",
           description:`Un lugar donde la cultura y el arte se entrelazan con la vida cotidiana. El municipio tiene una gran tradición musical, especialmente en bandas de viento que alegran sus fiestas y eventos, siendo un lugar donde el espíritu festivo caribeño se mantiene vivo y vibrante. Su enorme oferta gastronómica es icónica y representativa de la región, la cual incluye la Ciruela, la Almojábana, la Bandeja de Chicharrón, los Pasteles, el Guandú entre otros. Además de destacarse por su talento en las confecciones, ofreciendo una gran variedad de prendas de la mejor calidad.`,
           url:" https://tesorosdelatlantico.com/tesoro/baranoa/",
           img:[
-            "css/images/Turismo/Barranquilla/Barranquilla1.jpg",
-            "css/images/Turismo/Barranquilla/Barranquilla2.jpg",
-            "css/images/Turismo/Barranquilla/Barranquilla3.jpg",
-            "css/images/Turismo/Barranquilla/Barranquilla4.jpg"
+            "css/images/Turismo/4-BARANOA/6.BARANOA.jpg",
+            "css/images/Turismo/4-BARANOA/7.BARANOA.jpg"
           ],
         },
         geometry: {
@@ -112,15 +95,13 @@ var json_Puntosturisticos_3 = [
         type: "Feature",
         properties: {
           id: 2,
-          NOMBRE: "PONEDERA",
-          titular:"ENTRE MANOS QUE MOLDEAN SUEÑOS",
+          NOMBRE: "Ponedera",
+          titular:"Entre manos que moldean sueños",
           description:`Una pequeña joya bañada por el Río Magdalena. Con un estilo de vida tranquilo, es un lugar ideal para conectarse con la naturaleza. Sus fértiles tierras y el río ofrecen vistas hermosas y oportunidades para la pesca y actividades al aire libre. Los habitantes de Ponedera son conocidos por su amabilidad, además de su gran habilidad en la Alfarería y la preparación de los típicos Bollos, una cultura impregnada de tradiciones ancestrales.`,
           url:"https://tesorosdelatlantico.com/tesoro/ponedera/",
           img:[
-            "css/images/Turismo/Barranquilla/Barranquilla1.jpg",
-            "css/images/Turismo/Barranquilla/Barranquilla2.jpg",
-            "css/images/Turismo/Barranquilla/Barranquilla3.jpg",
-            "css/images/Turismo/Barranquilla/Barranquilla4.jpg"
+            "css/images/Turismo/5-PONEDERA/3.PONEDERA.jpg",
+            "css/images/Turismo/5-PONEDERA/4.PONEDERA.jpg"
           ],
         },
         geometry: {
@@ -141,15 +122,13 @@ var json_Puntosturisticos_3 = [
         type: "Feature",
         properties: {
           id: 2,
-          NOMBRE: "TUBARÁ",
-          titular:"TESORO SAGRADO DEL ATLÁNTICO",
+          NOMBRE: "Tubará",
+          titular:"Tesoro sagrado del atlántico",
           description:"Un lugar rodeado de exuberante naturaleza y paisajes costeros. Este municipio se enorgullece de sus raíces indígenas, siendo una de las pocas zonas donde aún habita la etnia Mokaná. Tubará es el hogar de hermosas playas como Puerto Velero, Caño Dulce y Puerto Caimán, que ofrecen espacios para practicar deportes náuticos y disfrutar del ecoturismo y el descanso. Las vistas desde sus montañas hacia el mar Caribe son espectaculares. Además, sus festividades reflejan un rico patrimonio cultural que conecta a sus pobladores con sus raíces ancestrales, creando una experiencia única para quienes lo visitan.",
           url:"https://tesorosdelatlantico.com/tesoro/tubara/",
           img:[
-            "https://i0.wp.com/notasdeactualidad.com/wp-content/uploads/2019/10/IMG-20191020-WA0044.jpg?w=1280&ssl=1",
-            "https://irp.cdn-website.com/760b2e87/dms3rep/multi/MPV-82.jpg",
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRoHmMYM1bdDD1m63bSqSemYvvv1_Artgfdw&s",
-            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRoHmMYM1bdDD1m63bSqSemYvvv1_Artgfdw&s",
+            "css/images/Turismo/6-TUBARA/4.TUBARA.jpg",
+            "css/images/Turismo/6-TUBARA/3.TUBARA.jpg"
           ],
         },
         geometry: {
